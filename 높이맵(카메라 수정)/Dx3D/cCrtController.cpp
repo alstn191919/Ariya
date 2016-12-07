f83cf09d60f7e3ff0b0b55a709536727de93cab8
chocoingan <yjhyun1537@naver.com>
2016-12-08 08:54:00 +0900

    카메라 수정

---
+++ 높이맵(카메라 수정)/Dx3D/cCrtController.cpp
@@ -0,0 +1,114 @@
+#include "StdAfx.h"
+#include "cCrtController.h"
+#include "iMap.h"
+
+cCrtController::cCrtController(void)
+	: m_vPosition(0, 0, 0)
+	, m_vDirection(0, 0, 1)
+	, m_fSpeed(0.3f)
+	, m_fAngleX(0.0f)
+	, m_isRight(false)
+
+{
+}
+
+
+cCrtController::~cCrtController(void)
+{
+}
+
+void cCrtController::Setup()
+{
+
+}
+
+void cCrtController::Update(iMap* pMap /*= NULL*/)
+{
+	D3DXMATRIXA16 matRx, matRy, mat;
+	D3DXMatrixRotationY(&matRx, m_fAngleX);
+	//D3DXMatrixRotationX(&matRy, m_fAngleY);
+	//D3DXMatrixRotationAxis(&matRy, &m_vDirection, m_fAngleY);
+
+	mat = matRx;
+	
+	D3DXVec3TransformNormal(&m_vDirection, &D3DXVECTOR3(0, 0, 1), &mat);
+
+	D3DXVECTOR3 tempDirection = m_vDirection;
+
+	if (GetKeyState('A') & 0x8000)
+	{
+		D3DXVec3TransformNormal(&tempDirection, &D3DXVECTOR3(-1, 0, 0), &mat);
+		m_vPosition += (tempDirection * (m_fSpeed - 0.2f));
+	}
+	if (GetKeyState('D') & 0x8000)
+	{
+		D3DXVec3TransformNormal(&tempDirection, &D3DXVECTOR3(1, 0, 0), &mat);
+		m_vPosition += (tempDirection * (m_fSpeed - 0.2f));
+	}
+	if(GetKeyState('S') & 0x8000)
+	{
+		if (GetKeyState('A') & 0x8000)
+		{
+			D3DXVec3TransformNormal(&tempDirection, &D3DXVECTOR3(-1, 0, 1), &mat);
+			m_vPosition -= (tempDirection * (m_fSpeed - 0.2f));
+		}
+		else if (GetKeyState('D') & 0x8000)
+		{
+			D3DXVec3TransformNormal(&tempDirection, &D3DXVECTOR3(1, 0, 1), &mat);
+			m_vPosition -= (tempDirection * (m_fSpeed - 0.2f));
+		}
+		else
+		m_vPosition -= (m_vDirection * (m_fSpeed - 0.2f));
+	}
+	if(GetKeyState('W') & 0x8000)
+	{
+		if (GetKeyState('A') & 0x8000)
+		{
+			D3DXVec3TransformNormal(&tempDirection, &D3DXVECTOR3(-1, 0, 1), &mat);
+			m_vPosition += (tempDirection * (m_fSpeed - 0.2f));
+		}
+		else if (GetKeyState('D') & 0x8000)
+		{
+			D3DXVec3TransformNormal(&tempDirection, &D3DXVECTOR3(1, 0, 1), &mat);
+			m_vPosition += (tempDirection * (m_fSpeed - 0.2f));
+		}
+		else
+		m_vPosition += (m_vDirection * (m_fSpeed - 0.1f));
+	}
+	//if (GetKeyState('A') & 0x8000)
+	//{
+	//	D3DXVec3TransformNormal(&m_vDirection, &D3DXVECTOR3(1, 0, 0), &mat);
+	//	m_vPosition += (m_vDirection * (m_fSpeed - 0.1f));
+	//}
+	//if (GetKeyState('D') & 0x8000)
+	//{
+	//	D3DXVec3TransformNormal(&m_vDirection, &D3DXVECTOR3(1, 0, 0), &mat);
+	//	m_vPosition -= (m_vDirection * (m_fSpeed - 0.1f));
+	//}
+	//if(GetKeyState('S') & 0x8000)
+	//{
+	//	//vPosition = m_vPosition + (m_vDirection * m_fSpeed);
+	//	//m_vDirection = D3DXVECTOR3(0, 0, 1);
+	//	m_vPosition -= (m_vDirection * (m_fSpeed - 0.2f));
+	//}
+	//if(GetKeyState('W') & 0x8000)
+	//{
+	//	//vPosition = m_vPosition - (m_vDirection * m_fSpeed);
+	//	//m_vDirection = D3DXVECTOR3(0, 0, 1);
+	//	m_vPosition += (m_vDirection * (m_fSpeed - 0.1f));
+	//}
+
+	//if (pMap)
+	//{
+	//	if(pMap->GetHeight(vPosition.x, vPosition.y, vPosition.z))
+	//		m_vPosition = vPosition;
+	//}
+	//else
+	//{
+	//	m_vPosition = vPosition;
+	//}
+
+	D3DXMATRIXA16 matT;
+	D3DXMatrixTranslation(&matT, m_vPosition.x, m_vPosition.y, m_vPosition.z);
+	m_matWorld =  mat * matT;
+}
